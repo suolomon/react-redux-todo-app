a0fa3325ae7f32edb12c50aedf745245a3cc5ec0
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-23 14:19:11 +0300

    add todoSlice

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,46 +1,61 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+import {saveTodo} from "../features/todoSlice";
 
 const Container = styled.div`
- height: 50px;    
- width: 80%; 
- border-radius: 12px; 
- background-color: white; 
- display: flex; 
- justify-content: center; 
- align-items: center;
-`
+  height: 50px;
+  width: 80%;
+  border-radius: 12px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const InputBox = styled.input`
- background: none; 
- border: none; 
- margin-left: 12px; 
- flex:1;
- outline: none;
- 
-`
+  background: none;
+  border: none;
+  margin-left: 12px;
+  flex: 1;
+  outline: none;
+`;
 
 const Button = styled.button`
- margin-right: 8px; 
- background-color: rgb(214, 213, 213);
- border:none; 
- border-radius: 10px; 
- height: 30px; 
- width: 50px;
-`
+  margin-right: 8px;
+  background-color: rgb(214, 213, 213);
+  border: none;
+  border-radius: 10px;
+  height: 30px;
+  width: 50px;
+`;
 
 const Input = () => {
-    const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
-    const addTodo = () => {
+  const addTodo = () => {
+    console.log(`Adding ${input}`);
 
-    }
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    );
+  };
 
-    return (
-        <Container>
-            <InputBox type="text" value={input} onChange={e => setInput(e.target.value)} />
-            <Button onClick={addTodo}>Add</Button>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <InputBox
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Button onClick={addTodo}>Add</Button>
+    </Container>
+  );
+};
 
-export default Input
+export default Input;
